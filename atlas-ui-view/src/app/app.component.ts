c6d525ecf4d097f3b21fd18243738575e97602e2
sofiane000 <maysof45@hotmail.com>
2018-11-11 16:35:18 -0500

    sidebar content

--- atlas-ui-view/src/app/app.component.ts
+++ atlas-ui-view/src/app/app.component.ts
@@ -1,9 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, ViewChild, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
+import { Router, NavigationEnd } from '@angular/router';
+import { AuthenticationService } from './core/services/authentication.service';
 
 @Component({
   selector: 'app-atlas',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+  isSideBarOpened: boolean;
+  isAuthorized: boolean;
+  isAuthorizedSubscription: Subscription;
+
+  isHandset: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+  @ViewChild('menu') menu: any;
+  @ViewChild('sidebar') sidebar: any;
+  @HostBinding('class.isAuthorized') addClass = false;
+  constructor(private breakpointObserver: BreakpointObserver, private router: Router,
+    private authenticationService: AuthenticationService) {
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if (event.url.search('sidebar')) {
+          // this.isSideBarOpened = true;
+        }
+      }
+    });
+    this.isAuthorizedSubscription = this.authenticationService.isAuthorized.subscribe(isAuthorized => {
+      if (isAuthorized) {
+        this.addClass = true;
+      }
+      this.isAuthorized = isAuthorized;
+    });
+    this.isAuthorized = this.authenticationService.isAuthenticated();
+    if (this.isAuthorized) {
+      this.addClass = true;
+    }
+  }
+  toggleSidebar() {
+    this.sidebar.toggle();
+  }
+  toggleMenu() {
+    this.menu.toggle();
+  }
+  ngOnDestroy(): void {
+    this.isAuthorizedSubscription.unsubscribe();
+  }
+
 }
