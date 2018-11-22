8c0c38e96309397e2405450738cd2ed32cdfa72f
sofiane000 <maysof45@hotmail.com>
2018-11-21 19:12:37 -0500

    Workaround for route change can deactivate

--- atlascomplete-web/src/app/modules/shared/services/can-deactivate.guard.ts
+++ atlascomplete-web/src/app/modules/shared/services/can-deactivate.guard.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
-import { CanDeactivate } from '@angular/router';
+import { CanDeactivate, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { ComponentCanDeactivate } from '../components/component-can-deactivate';
+import { Location } from '@angular/common';
 
 @Injectable()
 export class CanDeactivateGuard implements CanDeactivate<ComponentCanDeactivate> {
-    canDeactivate(component: ComponentCanDeactivate): boolean {
+    constructor(private readonly location: Location, private readonly route: Router) {
+
+    }
+    canDeactivate(component: ComponentCanDeactivate, currentRoute: ActivatedRouteSnapshot): boolean {
 
         if (!component.canDeactivate()) {
-            if (confirm('You have unsaved changes! If you leave, your changes will be lost.')) {
-                return true;
-            } else {
-                return false;
-            }
+            const currentUrlTree = this.route.createUrlTree([], currentRoute);
+            const currentUrl = currentUrlTree.toString();
+            this.location.go(currentUrl);
+            return confirm('You have unsaved changes! If you leave, your changes will be lost.');
+        } else {
+            return true;
         }
-        return true;
     }
 }
