398c548761f5bfa3d02a0c23c02a6d04ffc6a998
Aliaksandr Tarasevich <alxtrs@gmail.com>
2018-12-06 01:19:27 -0500

    Add example for batching

--- atlascomplete-web/src/app/modules/prod-admin/users/services/user.service.ts
+++ atlascomplete-web/src/app/modules/prod-admin/users/services/user.service.ts
@@ -7,32 +7,37 @@ import { DataAccessFactory, DataAccessService } from 'atlas-web-services';
 
 @Injectable()
 export class UserService extends AtlasGridService {
-  selectedUser: any;
-  dataAccess: DataAccessService;
+    selectedUser: any;
+    dataAccess: DataAccessService;
 
-  constructor(dataAccessFactory: DataAccessFactory) {
-    super();
-    this.dataAccess = dataAccessFactory.getService('idm.users');
-  }
+    constructor(dataAccessFactory: DataAccessFactory) {
+        super();
+        this.dataAccess = dataAccessFactory.getService('idm.users');
+    }
 
-  query(state: any): void {
-    this.fetch(state).subscribe(x => super.next(x));
-  }
+    query(state: any): void {
+        this.fetch(state).subscribe((x) => super.next(x));
+    }
 
-  fetch(state: any): Observable<any> {
-    this.isLoading = true;
-    return this.getUsers().pipe(tap(() => this.isLoading = false));
-  }
+    fetch(state: any): Observable<any> {
+        this.isLoading = true;
+        return this.getUsers().pipe(tap(() => (this.isLoading = false)));
+    }
 
-  getUsers() {
-    return this.dataAccess.get().pipe(map((response) => {
-      return response.body.data;
-    }));
-  }
+    getUsers() {
+        return this.dataAccess.get('', { pageSize: 2, restartRowId: '1' }).pipe(
+            map((response) => {
+                // restart row is in response.restartRowId
+                return response.body.data;
+            })
+        );
+    }
 
-  getUserById(loginSk: Number) {
-    return this.dataAccess.get(`${loginSk}`).pipe(map((response) => {
-      return response.body.data;
-    }));
-  }
+    getUserById(loginSk: Number) {
+        return this.dataAccess.get(`${loginSk}`).pipe(
+            map((response) => {
+                return response.body.data;
+            })
+        );
+    }
 }
