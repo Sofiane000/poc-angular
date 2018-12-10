5e9b354b0d13d1d702f8e12016ff18cfa3530e9f
Aliaksandr Tarasevich <alxtrs@gmail.com>
2018-12-10 08:14:08 -0500

    Add more tests + coverage

--- atlas-services/projects/atlas-web-services/src/lib/data-access/daataccess.service.spec.ts
+++ atlas-services/projects/atlas-web-services/src/lib/data-access/daataccess.service.spec.ts
@@ -1,17 +1,47 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { DataAccessFactory } from './dataaccess.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HttpClientModule } from '@angular/common/http';
 
 describe('DataAccessFactory', () => {
-    beforeEach(() =>
+    let service: DataAccessFactory;
+    beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [HttpClientModule],
-        })
-    );
+            imports: [HttpClientModule, HttpClientTestingModule],
+        });
+        service = TestBed.get(DataAccessFactory);
+        service.baseUrl = 'api';
+    });
 
     it('should be created', () => {
-        const service: DataAccessFactory = TestBed.get(DataAccessFactory);
         expect(service).toBeTruthy();
+    });
+
+    it('should be able to create a service endpoint', () => {
+        service
+            .createService('idm.users')
+            .module('idm')
+            .url('users/list');
+        expect(function() {
+            service.getService('idm.users');
+        }).not.toThrow();
+    });
+
+    it('should return to create a service data client', () => {
+        service
+            .createService('idm.members')
+            .module('idm')
+            .url('users/list');
+        expect(service.getService('idm.users')).toBeDefined();
+    });
+
+    describe('Should perform get operations', () => {
+        beforeEach(() => {
+            service
+                .createService('idm.users')
+                .module('idm')
+                .url('users/list');
+        });
     });
 });
