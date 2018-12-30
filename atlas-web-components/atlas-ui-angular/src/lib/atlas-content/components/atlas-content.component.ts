c0345255886971a9df84813288d74036cd9ceb21
sofiane000 <maysof45@hotmail.com>
2018-12-30 11:25:58 -0500

    get rid off old web-components

--- atlas-web-components/atlas-ui-angular/src/lib/atlas-content/components/atlas-content.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-    selector: 'atlas-content',
-    templateUrl: './atlas-content.component.html',
-    styleUrls: ['./atlas-content.component.scss'],
-})
-export class AtlasContentComponent {}
--- atlas-web-components/atlas-ui-angular/src/lib/layout/atlas-tab-layout/models/nav-link.ts
+++
@@ -1,5 +0,0 @@
-export interface INavLink {
-    label: string;
-    link: string;
-    index: number;
-}
